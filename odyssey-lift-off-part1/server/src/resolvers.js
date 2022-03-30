01fd4bbc82dee51a4ed4711fe220f5835f15fe4c
Jelly38214 <goolezheng@gmail.com>
2022-03-30 19:07:55 +0800

    feat: third part of lift-off -- use variable and resolvers chain.

--- odyssey-lift-off-part1/server/src/resolvers.js
+++ odyssey-lift-off-part1/server/src/resolvers.js
@@ -1,14 +1,22 @@
 const resolvers = {
   Query: {
-    tracksForHome:(parent, args, context, info) => {
+    // returns an array of Tracks that will be used to populate the homepage grid of our website
+    tracksForHome: (parent, args, context, info) => {
       return context.dataSources.trackAPI.getTracksForHome();
-    }
+    },
+    // get a single track by id, for the Track page.
+    track: (parent, args, context, info) => {
+      return context.dataSources.trackAPI.getTrack(args.id);
+    },
   },
   Track: {
     author: (parent, args, context, info) => {
-      return context.dataSources.trackAPI.getAuthor(parent.authorId)
-    }
-  }
+      return context.dataSources.trackAPI.getAuthor(parent.authorId);
+    },
+    modules: (parent, args, context, info) => {
+      return context.dataSources.trackAPI.getTrackModules(parent.id);
+    },
+  },
 };
 
 module.exports = resolvers;
